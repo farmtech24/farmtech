963ab50f7df88122876a3097af29443a2698702e
elmerurbina <[elmerurbina570@gmail.com]>
2024-03-15 18:44:14 -0600

    views added

--- backend/src/apps/api/cows/views.py
+++ backend/src/apps/api/cows/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CowForm
 
-# Create your views here.
+def upload_cow_photo(request):
+    if request.method == 'POST':
+        form = CowForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # Redirigir a una URL
+            return redirect('success_url_name')
+    else:
+        form = CowForm()
+    return render(request, 'cows/upload_cow_photo.html', {'form': form})
