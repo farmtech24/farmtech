b42762d43600bf57d1ed92fcade99c8064df9037
elmerurbina <[elmerurbina570@gmail.com]>
2024-03-15 18:23:34 -0600

    models added

--- backend/src/apps/api/cows/models.py
+++ backend/src/apps/api/cows/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Cow(models.Model):
+
+    # Campos para registrar las vacas
     CATEGORY_CHOICES = [
         ('crianza', 'Crianza'),
         ('lechera', 'Lechera'),
@@ -12,10 +14,13 @@ class Cow(models.Model):
     numero_partos = models.PositiveIntegerField(default=0)
     lote = models.CharField(max_length=100)
     codigo_unico = models.CharField(max_length=100, blank=True, null=True)
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='productora')
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='lechera')
 
     # Agregar campos adicionales
     additional_fields = models.JSONField(blank=True, null=True)
+
+     # Permite agregar una foto de la vaca
+    photo = models.ImageField(upload_to='cow_photos/', blank=True, null=True)
 
     def __str__(self):
         return self.nombre
