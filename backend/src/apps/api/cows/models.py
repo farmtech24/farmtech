699d63d041c7af0bb7e12b7c8aaae0c100e3fc06
elmerurbina <[elmerurbina570@gmail.com]>
2024-03-15 18:20:32 -0600

    API apps created

--- backend/src/apps/api/cows/models.py
+++ backend/src/apps/api/cows/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Cow(models.Model):
+    CATEGORY_CHOICES = [
+        ('crianza', 'Crianza'),
+        ('lechera', 'Lechera'),
+    ]
+
+    arete = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    nacimiento = models.DateField()
+    numero_partos = models.PositiveIntegerField(default=0)
+    lote = models.CharField(max_length=100)
+    codigo_unico = models.CharField(max_length=100, blank=True, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='productora')
+
+    # Agregar campos adicionales
+    additional_fields = models.JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
