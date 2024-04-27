db742bbc69bd00118ceae5a73fcf25c9999122d1
elmerurbina <[elmerurbina570@gmail.com]>
2024-04-26 18:08:58 -0600

    files update

--- frontend/myApp.py
+++ frontend/myApp.py
@@ -36,6 +36,29 @@ class CircleButton(ButtonBehavior, Label):
     pass
 
 
+class MilkRecordScreen(Screen):
+    def __init__(self, **kwargs):
+        super(MilkRecordScreen, self).__init__(**kwargs)
+        self.add_widgets()
+
+    def add_widgets(self):
+        layout = BoxLayout(orientation='vertical')
+
+        # Welcome message
+        welcome_label = Label(text="Bienvenido a Registro de Leche", font_size='20sp')
+        layout.add_widget(welcome_label)
+
+        # Button for "Crear Nueva Hoja de Registro"
+        create_record_button = Button(text="Crear Nueva Hoja de Registro",
+                                      on_release=self.create_new_record)
+        layout.add_widget(create_record_button)
+
+        self.add_widget(layout)
+
+    def create_new_record(self, instance):
+        print("Creating a new milk record...")
+
+
 class MainApp(App):
     def build(self):
         # Configuring window size
@@ -52,17 +75,19 @@ class MainApp(App):
         sm.add_widget(MilkProductionScreen(name='ranchs'))
         sm.add_widget(CostStructureScreen(name='cost_structure'))
         sm.add_widget(ProductionPlanScreen(name='production_plan'))
+        sm.add_widget(MilkRecordScreen(name='registro_de_leche'))
 
         # Main layout
         layout = BoxLayout(orientation='vertical')
 
         # Menu
         menu_layout = GridLayout(cols=5, size_hint_y=None, height=50, spacing=10)  # Add spacing between buttons
-        menu_layout.add_widget(Button(text='Estructura de costos', on_release=lambda x: sm.current_screen(name='Cost Structure'),
-                                      background_color=(0.6, 0.8, 1, 1),  # Set background color to light blue
-                                      font_size='13sp'))  # Adjust font size
         menu_layout.add_widget(
-            Button(text='Plan de produccion', on_release=lambda x: sm.current_screen(name='Production Plan'),
+            Button(text='Estructura de costos', on_release=lambda x: self.change_screen(sm, 'cost_structure'),
+                   background_color=(0.6, 0.8, 1, 1),  # Set background color to light blue
+                   font_size='13sp'))  # Adjust font size
+        menu_layout.add_widget(
+            Button(text='Plan de produccion', on_release=lambda x: self.change_screen(sm, 'production_plan'),
                    background_color=(0.6, 0.8, 1, 1),  # Set background color to light blue
                    font_size='14sp'))  # Adjust font size
 
@@ -75,10 +100,11 @@ class MainApp(App):
 
         # Adding buttons
         buttons_layout = BoxLayout(orientation='vertical', spacing=10, size_hint=(None, None), width=200, height=300)
-        options = ['Fincas', 'Perfil', 'Registro de Leche', 'Manejo de Inventarios', 'Suscripciones', 'Notas', 'Ayuda']
+        options = ['Perfil', 'Registro de Leche', 'Manejo de Inventarios', 'Suscripciones', 'Notas',
+                   'Ayuda']
         for option in options:
             buttons_layout.add_widget(
-                Button(text=option, on_release=lambda x, option=option: self.open_module(option),
+                Button(text=option, on_release=lambda x, option=option: self.change_screen(sm, option),
                        size_hint=(None, None), width=180, height=40))
 
         content_layout.add_widget(buttons_layout)
@@ -88,8 +114,12 @@ class MainApp(App):
 
         return layout
 
-    def open_module(self, module_name):
-        print(f'Opening {module_name}')
+    def change_screen(self, sm, screen_name):
+        print(f"Changing screen to: {screen_name}")
+        if screen_name == 'Registro de Leche':
+            sm.current = 'registro_de_leche'
+        else:
+            sm.current = screen_name
 
 
 if __name__ == '__main__':
